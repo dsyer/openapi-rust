06325c7e550f6cf8b45dbeb57f42989b2738b0fa
Dave Syer <dsyer@vmware.com>
2022-05-20 09:05:00 +0100

    Ensure node.js works

--- image/src/lib.rs
+++ image/src/lib.rs
@@ -4,14 +4,6 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 
-use std::collections::BTreeMap;
-
-use std::str;
-
-use k8s_openapi::api::apps::v1::*;
-use k8s_openapi::api::core::v1::*;
-use k8s_openapi::apimachinery::pkg::apis::meta::v1::*;
-
 use wasm_bindgen::prelude::wasm_bindgen;
 
 mod models;
@@ -20,6 +12,8 @@ use models::*;
 
 #[wasm_bindgen]
 pub fn xform(json: &str) -> String {
-    let mut image: V1Image = serde_json::from_str(json).unwrap();
-    return serde_json::to_string(&image).unwrap();
+    let image: V1Image = serde_json::from_str(json).unwrap();
+    let mut status = V1ImageStatus::new();
+    status.complete = Some(false);
+    return serde_json::to_string(&status).unwrap();
 }
